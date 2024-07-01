9984f44c34904c8ef598680c34d41c58f0660714
James Lee-Jones <james@lee-jones.co.uk>
2024-07-01 22:43:15 +0100

    Remove compile command generation

--- Experiments/setup.sh
+++ Experiments/setup.sh
@@ -51,7 +51,7 @@ for opt_level in "O0" "O1" "O2" "O3"; do
         "$DREDD_EVAL/setup_scripts/compile-tint.sh"
       popd
 
-      "$DREDD"/dredd --semantics-preserving-coverage-instrumentation -p ./out/Debug/compile_commands.json --mutation-info-file ../mutation_info_file.json $(python3 $DREDD_EVAL/utils/get_compile_command_files.py --ignore-tests ./out/Debug/compile_commands.json ./src .cc .c .h --ignore ./src/tint/lang/wgsl/reader/parser/parser.cc)
+      "$DREDD"/dredd --semantics-preserving-coverage-instrumentation -p ./out/Debug/compile_commands.json $(python3 $DREDD_EVAL/utils/get_compile_command_files.py --ignore-tests ./out/Debug/compile_commands.json ./src .cc .c .h --ignore ./src/tint/lang/wgsl/reader/parser/parser.cc)
       # TODO(JLJ): Check it is fine to do this and no files are left over elsewhere
       rm -rf out/Debug/*
     popd
@@ -78,7 +78,7 @@ for opt_level in "O0" "O1" "O2" "O3"; do
         "$DREDD_EVAL/setup_scripts/compile-binutils.sh"
       popd
 
-      "$DREDD"/dredd --semantics-preserving-coverage-instrumentation -p ./objdir/compile_commands.json --mutation-info-file ../mutation_info_file.json $(python3 $DREDD_EVAL/utils/get_project_source_files.py $DREDD_EVAL/evaluation_setup.yaml binutils)
+      "$DREDD"/dredd --semantics-preserving-coverage-instrumentation -p ./objdir/compile_commands.json $(python3 $DREDD_EVAL/utils/get_project_source_files.py $DREDD_EVAL/evaluation_setup.yaml binutils)
       # TODO(JLJ): Check it is fine to do this and no files are left over elsewhere
       rm -rf objdir/*
     popd
