ef034400436b0d0be494545e66985049b25bd5fe
James Lee-Jones <james@lee-jones.co.uk>
2024-07-01 16:09:25 +0100

    Fix Experiment setup

--- Experiments/full-experiment-run.sh
+++ Experiments/full-experiment-run.sh
@@ -7,7 +7,7 @@ results_dir=${1:-"$DREDD_EVAL/Experiments/results/"}
 NUM_RUNS=3
 
 pushd "$DREDD_EVAL"/Experiments
-  ./setup.sh "$results_dir"
+  ./setup.sh
   ./cleanup.sh
 
   for ((i=1; i<NUM_RUNS; i++)); do
