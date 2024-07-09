afc2ea7900cc3f66240bbe7b3e465c4661a31919
James Lee-Jones <james@lee-jones.co.uk>
2024-07-09 18:48:06 +0100

    Update evaluation setup

--- setup_scripts/setup.sh
+++ setup_scripts/setup.sh
@@ -15,5 +15,8 @@ cd "$DREDD_EVAL"/Evaluation
 # Setup tint for evaluation
 "$DREDD_EVAL"/setup_scripts/setup-tint.sh
 
+# Setup spirv-tools for evaluation
+"$DREDD_EVAL"/setup_scripts/setup-tint.sh
+
 # Setup binutils for evaluation
 "$DREDD_EVAL"/setup_scripts/setup-binutils.sh
