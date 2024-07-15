44434d09b658d19c553b3ac78998f49affe2bc86
James Lee-Jones <james@lee-jones.co.uk>
2024-07-15 17:24:43 +0100

    Refactor setup-thirdparty

--- setup_scripts/setup-thirdparty.sh
+++ setup_scripts/setup-thirdparty.sh
@@ -8,41 +8,40 @@ pushd "$DREDD_EVAL"/third_party
   # Setup Dredd
   if [ ! -d "dredd" ]; then
     git clone --recursive https://github.com/mc-imperial/dredd.git
+
+    pushd dredd
+      pushd third_party
+      if [ "$(ls -A clang+llvm)" ]; then
+        rm -rf clang+llvm
+        curl -Lo clang+llvm.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
+        tar xf clang+llvm.tar.xz
+        mv clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04 clang+llvm
+        rm clang+llvm.tar.xz
+      fi
+      popd  # third_party
+
+      git checkout semantics-preserving-code-coverage
+
+      mkdir -p build
+      pushd build
+        cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release
+        cmake --build . --config Release
+        cp src/dredd/dredd ../third_party/clang+llvm/bin
+      popd  # build
+    popd  # dredd
   fi
-
-  pushd dredd
-    pushd third_party
-    if [ "$(ls -A clang+llvm)" ]; then
-      rm -rf clang+llvm
-      curl -Lo clang+llvm.tar.xz https://github.com/llvm/llvm-project/releases/download/llvmorg-16.0.4/clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04.tar.xz
-      tar xf clang+llvm.tar.xz
-      mv clang+llvm-16.0.4-x86_64-linux-gnu-ubuntu-22.04 clang+llvm
-      rm clang+llvm.tar.xz
-    fi
-    popd  # third_party
-
-    git checkout semantics-preserving-code-coverage
-
-    mkdir -p build
-    pushd build
-      cmake -G Ninja .. -DCMAKE_BUILD_TYPE=Release
-      cmake --build . --config Release
-      cp src/dredd/dredd ../third_party/clang+llvm/bin
-    popd  # build
-  popd  # dredd
-
 
   # Setup AFL++
   if [ ! -d "AFLplusplus" ]; then
     git clone https://github.com/AFLplusplus/AFLplusplus
+
+    export LLVM_CONFIG=llvm-config-16
+
+    pushd AFLplusplus
+      make
+      sudo make install
+    popd  # AFLplusplus
   fi
-  export LLVM_CONFIG=llvm-config-16
-
-  pushd AFLplusplus
-    make
-    sudo make install
-  popd  # AFLplusplus
-
 
   # Setup afl-cov
   if [ ! -d "afl-cov" ]; then
